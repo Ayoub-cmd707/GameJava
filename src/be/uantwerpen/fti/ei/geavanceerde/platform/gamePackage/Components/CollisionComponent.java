5b6d33d792d32397d2df4d407149276f0a299004
Ayoub-cmd707 <45266255+Ayoub-cmd707@users.noreply.github.com>
2022-06-20 11:05:12 +0200

    elements fix

--- src/be/uantwerpen/fti/ei/geavanceerde/platform/gamePackage/Components/CollisionComponent.java
+++ src/be/uantwerpen/fti/ei/geavanceerde/platform/gamePackage/Components/CollisionComponent.java
@@ -3,106 +3,39 @@ package be.uantwerpen.fti.ei.geavanceerde.platform.gamePackage.Components;
 import be.uantwerpen.fti.ei.geavanceerde.platform.gamePackage.Game;
 import be.uantwerpen.fti.ei.geavanceerde.platform.helper.ConfigFileReader;
 
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
 
 public class CollisionComponent {
-    private String settingsFile;
+   protected float x,y;
+   protected int width, height;
+   protected Rectangle hitBox;
 
 
-    /*public CollisionDetectionComponent(String configFile){
-        this.settingsFile = configFile;
+    public CollisionComponent(float x, float y, int width, int height ) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        initHitbox();
+        
+    }
+
+    private void initHitbox() {
+        hitBox = new Rectangle((int) x,(int) y,width,height);
+    }
+
+    protected void updateHitbox(){
+        hitBox.x = (int) x;
+        hitBox.y = (int) y;
+    }
+
+    public Rectangle getHitBox(){
+        return hitBox;
     }
 
 
-    public boolean CanMoveHere(float x,float y, float width, float height, int[][] levelData){
-        if(!IsSolid(x,y,levelData)){
-            if(!IsSolid(x+width,y+height,levelData)){
-                if(!IsSolid(x+width,y,levelData)){
-                    if(!IsSolid(x,y+height,levelData)){
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
 
 
-    private boolean IsSolid(float x, float y, int[][] levelData){
-        HashMap<String, Integer> data = ConfigFileReader.getConfigFileReaderInstance().loadOrCreateConfig(settingsFile);
-
-        if(x < 0.0 || x >= data.get("ScreenWidth")){
-            return true;
-        }
-        if(y < 0.0 || y >= data.get("ScreenHeight")){
-            return true;
-        }
-
-        float xIndex = x / Game.TILES_SIZE;
-        float yIndex = y / Game.TILES_SIZE;
-
-        int value  =levelData[(int) yIndex][(int)xIndex];
-
-
-        if(value!=0 && value != 2 && value != 4 && value != 7){return true;}
-        return false;
-    }
-
-    public float GetEntityPosNextToWall(int x, int y, int width, int height, Float xSpeed){
-        Rectangle2D.Float hitbox = new Rectangle2D.Float();
-        hitbox.x = x;
-        hitbox.y = y;
-        hitbox.width = width;
-        hitbox.height = height;
-        int currentTile = (int)(hitbox.x / Game.TILES_SIZE);
-        if(xSpeed > 0){
-            //right
-            int tileXpos = currentTile * Game.TILES_SIZE;
-            int xOffset = (int)(Game.TILES_SIZE - hitbox.width);
-            System.out.println(tileXpos + xOffset - 1);
-            return tileXpos + xOffset - 1;
-        }
-        else{
-            //left
-            return currentTile * Game.TILES_SIZE;
-        }
-
-    }
-
-    public float GetEntityYPosUnderRoofOrAboveFloor(int x, int y, int width, int height, Float airSpeed){
-        Rectangle2D.Float hitbox = new Rectangle2D.Float();
-        hitbox.x = x;
-        hitbox.y = y;
-        hitbox.width = width;
-        hitbox.height = height;
-        int currentTile = (int) (hitbox.y / Game.TILES_SIZE);
-        if(airSpeed > 0){
-            //falling or touching floor
-            int tileYPos = currentTile * Game.TILES_SIZE;
-            int yOffset = (int)(Game.TILES_SIZE - hitbox.height);
-            return tileYPos + yOffset - 1;
-        }
-        else{
-            //jumping
-            return currentTile * Game.TILES_SIZE;
-        }
-    }
-
-
-    public boolean IsEntityOnFloor(int x,int y, int width, int height, int[][] levelData){
-        Rectangle2D.Float hitbox = new Rectangle2D.Float();
-        hitbox.x = x;
-        hitbox.y = y;
-        hitbox.width = width;
-        hitbox.height = height;
-        //System.out.println(hitbox);
-        //check below bottomleft and bottomright
-        if(!IsSolid(hitbox.x, hitbox.y + hitbox.height+1, levelData)){
-            if(!IsSolid(hitbox.x + hitbox.width,hitbox.y + hitbox.height+1, levelData)){
-                return false;
-            }
-        }
-        return true;
-    }*/
 }
--- src/be/uantwerpen/fti/ei/geavanceerde/platform/gamePackage/enteties/AbstractPlayer.java
+++ src/be/uantwerpen/fti/ei/geavanceerde/platform/gamePackage/enteties/AbstractPlayer.java
@@ -1,12 +1,38 @@
 package be.uantwerpen.fti.ei.geavanceerde.platform.gamePackage.enteties;
 
-import be.uantwerpen.fti.ei.geavanceerde.platform.gamePackage.AbstractElement;
+import be.uantwerpen.fti.ei.geavanceerde.platform.gamePackage.Components.MovementComponent;
+import be.uantwerpen.fti.ei.geavanceerde.platform.gamePackage.Components.PositioningComponent;
+import be.uantwerpen.fti.ei.geavanceerde.platform.gamePackage.Drawable;
 
-public abstract class AbstractPlayer extends AbstractElement {
+public abstract class AbstractPlayer implements Drawable {
 
-    public AbstractPlayer(int x, int y, int width, int height) {
-        super(x, y, width, height);
+    private final PositioningComponent positionComponent;
+    private final MovementComponent movementComponent;
+    private AbstractInput.Inputs input;
+
+    public AbstractPlayer(int x, int y , int width, int height){
+        this.positionComponent = new PositioningComponent(x,y,height,width);
+        this.movementComponent = new MovementComponent();
     }
+
+
+    public PositioningComponent getPosition(){
+        return  positionComponent;
+    }
+
+    public MovementComponent getMovement(){
+        return movementComponent;
+    }
+
+    public AbstractInput.Inputs getInput(){
+        return input;
+    }
+
+    public void setDirection(AbstractInput.Inputs input) {
+        this.input = input;
+    }
+
+
 
 
 }
