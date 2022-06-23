560987aa9df16e5343d48483e4dd23e1af83596d
Ayoub <s102331@ap.be>
2022-06-23 22:06:33 +0200

    kleine test

--- src/be/uantwerpen/fti/ei/geavanceerde/platform/visualistationPackage/j2dInputs.java
+++ src/be/uantwerpen/fti/ei/geavanceerde/platform/visualistationPackage/j2dInputs.java
@@ -10,39 +10,53 @@ import java.util.LinkedList;
 import java.util.Map;
 
 public class j2dInputs extends AbstractInput{
-    private final LinkedList<Inputs> keyValues;
+    private static HashMap<String, Boolean> keyValues;
+
     public j2dInputs(JFrame frame) {
         frame.addKeyListener(new KeyInputAdapter());
-        keyValues = new LinkedList<>();
+        keyValues = new HashMap<>();
+        keyValues.put("LEFT",false);
+        keyValues.put("RIGHT",false);
+        keyValues.put("ATTACKING",false);
+        keyValues.put("JUMPING",false);
+        //System.out.println(keyValues);
     }
 
     @Override
     public Inputs getInputs() {
-        if (!keyValues.isEmpty())
-            return keyValues.poll();
+        for (Map.Entry<String, Boolean> me : keyValues.entrySet()) {
+            if(me.getKey().equals("LEFT") && me.getValue().equals(true)){return Inputs.LEFT;}
+            if(me.getKey().equals("RIGHT") && me.getValue().equals(true)){return Inputs.RIGHT;}
+            if(me.getKey().equals("JUMPING") && me.getValue().equals(true)){return Inputs.JUMP;}
+        }
         return Inputs.IDLE;
+
+        //CURRENTLY ONE INPUT IS DETECTED NEED TO ADD FUNCTIONALITY TO DETECT 2 INPUTS LIKE RUNNING AND SHOOTING
+
     }
 
 
+    static class KeyInputAdapter extends KeyAdapter {
 
-    class KeyInputAdapter extends KeyAdapter {
         @Override
-        public void keyPressed(KeyEvent e) {
-            int keycode = e.getKeyCode();
-            switch (keycode) {
-                case KeyEvent.VK_LEFT : keyValues.add(Inputs.LEFT);  break;
-                case KeyEvent.VK_RIGHT: keyValues.add(Inputs.RIGHT); break;
-                case KeyEvent.VK_SPACE: keyValues.add(Inputs.JUMP); break;
-            }
+        public void keyTyped(KeyEvent e) {
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
             switch (e.getKeyCode()) {
-                case KeyEvent.VK_LEFT :
-                case KeyEvent.VK_SPACE:
-                case KeyEvent.VK_RIGHT:
-                    keyValues.clear();  break;
+                case KeyEvent.VK_LEFT -> keyValues.put("LEFT",false);
+                case KeyEvent.VK_RIGHT -> keyValues.put("RIGHT",false);
+                case KeyEvent.VK_SPACE -> keyValues.put("JUMPING",false);
+            }
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT -> keyValues.put("LEFT",true);
+                case KeyEvent.VK_RIGHT -> keyValues.put("RIGHT",true);
+                case KeyEvent.VK_SPACE -> keyValues.put("JUMPING",true);
             }
         }
     }
